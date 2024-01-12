87d256fd4560f1a063f386bb17164d8536e5bb77
abadem17 <abad_zer0@hotmail.com>
2024-01-12 12:54:38 -0600

    se agrego case1
    Se agrego la opcion de calcular area de rectangulo en la clase main

--- Eje9_9/Main.java
+++ Eje9_9/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
         Scanner entrada = new Scanner(System.in);
         int dato;
+        double lado1=0,lado2=0,lado3=0;
+
 
         do {
             System.out.println("Que tipo de poligono desea ingresar? ");
@@ -18,10 +20,22 @@ public class Main {
             dato = entrada.nextInt();
             switch (dato){
                 case 1:
+                    System.out.println("Digite el primer lado del rectangulo");
+                    lado1 = entrada.nextDouble();
+                    System.out.println("Digite el segundo lado del rectangulo");
+                    lado2 = entrada.nextDouble();
+                    Rectangulo rectangulo = new Rectangulo(4,lado1 ,lado2);
+                    double area = rectangulo.area();
+
+                    poligono.add(rectangulo);
+                    System.out.println(rectangulo.toString()+"\nArea= "+area);
+
                     break;
                 case 2:
                     break;
                 case 3:
+                    break;
+                case 4:
                     break;
                 default:
                     System.out.println("Numero incorrecto, ingrese nuevamente.");
